09d473be0c0e46aeed865794b393a91c895a3b71
Bastian Joe <bastinjoe64@gmail.com>
2018-06-29 12:49:29 +0530

    Minified Js

--- src/main/webapp/Assets/js/login.js
+++ src/main/webapp/Assets/js/login.js
@@ -1,163 +1,87 @@
-var SVGLoader = '<svg version="1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16px" height="16px" viewBox="0 0 16 16"> <style type="text/css">.qp-circular-loader{width:16px;height:16px;-webkit-animation-name:rotate;-webkit-animation-duration:1.568s;-webkit-animation-iteration-count:infinite;-webkit-animation-timing-function:linear}.qp-circular-loader-path{stroke-dasharray:32.4;stroke-dashoffset:32.4;-webkit-animation-name:fillunfill,rot,colors;-webkit-animation-duration:1333ms,5332ms,5332ms;-webkit-animation-iteration-count:infinite,infinite,infinite;-webkit-animation-timing-function:cubic-bezier(.4,0,.2,1),steps(4),linear;-webkit-animation-play-state:running,running,running;-webkit-animation-fill-mode:forwards}.qp-circular-loader,.qp-circular-loader *{-webkit-transform-origin:50% 50%}@-webkit-keyframes rotate{from{-webkit-transform:rotate(0)}to{-webkit-transform:rotate(360deg)}}@-webkit-keyframes fillunfill{from{stroke-dashoffset:32.3}50%{stroke-dashoffset:0}to{stroke-dashoffset:-31.9}}@-webkit-keyframes rot{from{-webkit-transform:rotate(0)}to{-webkit-transform:rotate(-360deg)}}@-webkit-keyframes colors{from,to{stroke:#FFF}}</style> <g class="qp-circular-loader"> <path class="qp-circular-loader-path" fill="none" d="M 8,1.125 A 6.875,6.875 0 1 1 1.125,8" stroke-width="2.25" stroke-linecap="round"></path> </g> </svg>&emsp;<i>loading...</i>';
-        var TmpHtml = '';
-        $(function() {
-        $("#login").submit(function() {
-        $(".panel-sign-in").addClass("hidden");
-                $(".panel-sign-in-loader").removeClass("hidden");
-                var $form = $(this);
-                $.ajax({
-                url: "DoAuth",
-                        type: 'POST',
-                        data: $form.serialize(),
-//                        data: {
-//                        userName: "1272011@tcs.com",
-//                                password: "Tcs#1234"
-//
-//                        },
-                        success: function(data) {
-                        console.log(data);
-                                if (data.responseCode === 1 && data.doauth === true) {
-                        $.notify("Redirecting", "success");
-//                        console.log("hi");
-                               window.location.href = contextPath + "/views/" + data.role + "/";
-                        } else {
-                        $.notify("Invalid Credentials", "error");
-                                $(".panel-sign-in").removeClass("hidden");
-                                $(".panel-sign-in-loader").addClass("hidden");
-                        }
-                        }
-                });
-                return false;
-        });
-        });
-//pixie canvas background
-        function BrowserDetection() {
-        if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-        var e = new Number(RegExp.$1);
-                rint = 10;
-                pulsion = 3
-        } else if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
-        var t = new Number(RegExp.$1);
-                rint = 50;
-                pulsion = 16
-        } else if (/Chrome[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-        var n = new Number(RegExp.$1);
-                rint = 50;
-                pulsion = 16
-        } else if (/Opera[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-        var r = new Number(RegExp.$1);
-                rint = 50;
-                pulsion = 16
-        } else if (/Safari[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
-        var i = new Number(RegExp.$1);
-                rint = 50;
-                pulsion = 16
-        }
-        WIDTH = window.innerWidth;
-                if (WIDTH < 1024) {
-        numberObject = 19e3;
-                numberBall = 10;
-                pulsion = 2;
-                rint = 10
-        }
-        }
+var WIDTH, HEIGHT, canvas, con, g, SVGLoader = '<svg version="1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16px" height="16px" viewBox="0 0 16 16"> <style type="text/css">.qp-circular-loader{width:16px;height:16px;-webkit-animation-name:rotate;-webkit-animation-duration:1.568s;-webkit-animation-iteration-count:infinite;-webkit-animation-timing-function:linear}.qp-circular-loader-path{stroke-dasharray:32.4;stroke-dashoffset:32.4;-webkit-animation-name:fillunfill,rot,colors;-webkit-animation-duration:1333ms,5332ms,5332ms;-webkit-animation-iteration-count:infinite,infinite,infinite;-webkit-animation-timing-function:cubic-bezier(.4,0,.2,1),steps(4),linear;-webkit-animation-play-state:running,running,running;-webkit-animation-fill-mode:forwards}.qp-circular-loader,.qp-circular-loader *{-webkit-transform-origin:50% 50%}@-webkit-keyframes rotate{from{-webkit-transform:rotate(0)}to{-webkit-transform:rotate(360deg)}}@-webkit-keyframes fillunfill{from{stroke-dashoffset:32.3}50%{stroke-dashoffset:0}to{stroke-dashoffset:-31.9}}@-webkit-keyframes rot{from{-webkit-transform:rotate(0)}to{-webkit-transform:rotate(-360deg)}}@-webkit-keyframes colors{from,to{stroke:#FFF}}</style> <g class="qp-circular-loader"> <path class="qp-circular-loader-path" fill="none" d="M 8,1.125 A 6.875,6.875 0 1 1 1.125,8" stroke-width="2.25" stroke-linecap="round"></path> </g> </svg>&emsp;<i>loading...</i>',
+    TmpHtml = "";
+
+function BrowserDetection() {
+    if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+        new Number(RegExp.$1);
+        rint = 10, pulsion = 3
+    } else if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
+        new Number(RegExp.$1);
+        rint = 50, pulsion = 16
+    } else if (/Chrome[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+        new Number(RegExp.$1);
+        rint = 50, pulsion = 16
+    } else if (/Opera[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+        new Number(RegExp.$1);
+        rint = 50, pulsion = 16
+    } else if (/Safari[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+        new Number(RegExp.$1);
+        rint = 50, pulsion = 16
+    }(WIDTH = window.innerWidth) < 1024 && (numberObject = 19e3, numberBall = 10, pulsion = 2, rint = 10)
+}
+
 function draw() {
-con.clearRect(0, 0, WIDTH, HEIGHT);
-        for (var e = 0; e < pxs.length; e++) {
-pxs[e].fade();
-        pxs[e].move();
-        pxs[e].draw()
-        }
+    con.clearRect(0, 0, WIDTH, HEIGHT);
+    for (var t = 0; t < pxs.length; t++) pxs[t].fade(), pxs[t].move(), pxs[t].draw()
 }
+
 function Circle() {
-WIDTH = window.innerWidth;
-        HEIGHT = window.innerHeight;
-        this.s = {
+    WIDTH = window.innerWidth, HEIGHT = window.innerHeight, this.s = {
         ttl: numberObject,
-                xmax: speedX,
-                ymax: speedY,
-                rmax: radius,
-                rt: pulsion,
-                xdef: 960,
-                ydef: 540,
-                xdrift: 4,
-                ydrift: 4,
-                random: true,
-                blink: true
-        };
-        this.reset = function() {
-        this.x = this.s.random ? WIDTH * Math.random() : this.s.xdef;
-                this.y = this.s.random ? HEIGHT * Math.random() : this.s.ydef;
-                this.r = (this.s.rmax - 1) * Math.random() + 1;
-                this.dx = Math.random() * this.s.xmax * (Math.random() < .5 ? - 1 : 1);
-                this.dy = Math.random() * this.s.ymax * (Math.random() < .5 ? - 1 : 1);
-                this.hl = this.s.ttl / rint * (this.r / this.s.rmax);
-                this.rt = Math.random() * this.hl;
-                this.s.rt = Math.random() + 1;
-                this.stop = Math.random() * .2 + .4;
-                this.s.xdrift *= Math.random() * (Math.random() < .5 ? - 1 : 1);
-                this.s.ydrift *= Math.random() * (Math.random() < .5 ? - 1 : 1)
-        };
-        this.fade = function() {
+        xmax: speedX,
+        ymax: speedY,
+        rmax: radius,
+        rt: pulsion,
+        xdef: 960,
+        ydef: 540,
+        xdrift: 4,
+        ydrift: 4,
+        random: !0,
+        blink: !0
+    }, this.reset = function() {
+        this.x = this.s.random ? WIDTH * Math.random() : this.s.xdef, this.y = this.s.random ? HEIGHT * Math.random() : this.s.ydef, this.r = (this.s.rmax - 1) * Math.random() + 1, this.dx = Math.random() * this.s.xmax * (Math.random() < .5 ? -1 : 1), this.dy = Math.random() * this.s.ymax * (Math.random() < .5 ? -1 : 1), this.hl = this.s.ttl / rint * (this.r / this.s.rmax), this.rt = Math.random() * this.hl, this.s.rt = Math.random() + 1, this.stop = .2 * Math.random() + .4, this.s.xdrift *= Math.random() * (Math.random() < .5 ? -1 : 1), this.s.ydrift *= Math.random() * (Math.random() < .5 ? -1 : 1)
+    }, this.fade = function() {
         this.rt += this.s.rt
-        };
-        this.draw = function() {
-        if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl))
-                this.s.rt = this.s.rt * - 1;
-                else if (this.rt >= this.hl)
-                this.reset();
-                var e = 1 - this.rt / this.hl;
-                con.beginPath();
-                con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
-                con.closePath();
-                var t = this.r * e;
-                g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, t <= 0 ? 1 : t);
-                g.addColorStop(1, colorRect + e * opacityRect + ")");
-                con.fillStyle = g;
-                con.fill()
-        };
-        this.move = function() {
-        WIDTH = window.innerWidth;
-                HEIGHT = window.innerHeight;
-                this.x += this.rt / this.hl * this.dx;
-                this.y += this.rt / this.hl * this.dy;
-                if (this.x > WIDTH || this.x < 0)
-                this.dx *= - 1;
-                if (this.y > HEIGHT || this.y < 0)
-                this.dy *= - 1
-        };
-        this.getX = function() {
+    }, this.draw = function() {
+        this.s.blink && (this.rt <= 0 || this.rt >= this.hl) ? this.s.rt = -1 * this.s.rt : this.rt >= this.hl && this.reset();
+        var t = 1 - this.rt / this.hl;
+        con.beginPath(), con.arc(this.x, this.y, this.r, 0, 2 * Math.PI, !0), con.closePath();
+        var i = this.r * t;
+        (g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, i <= 0 ? 1 : i)).addColorStop(1, colorRect + t * opacityRect + ")"), con.fillStyle = g, con.fill()
+    }, this.move = function() {
+        WIDTH = window.innerWidth, HEIGHT = window.innerHeight, this.x += this.rt / this.hl * this.dx, this.y += this.rt / this.hl * this.dy, (this.x > WIDTH || this.x < 0) && (this.dx *= -1), (this.y > HEIGHT || this.y < 0) && (this.dy *= -1)
+    }, this.getX = function() {
         return this.x
-        };
-        this.getY = function() {
+    }, this.getY = function() {
         return this.y
-        }
+    }
 }
-var WIDTH;
-        var HEIGHT;
-        var canvas;
-        var con;
-        var g;
-        var pxs = new Array;
-        var numberObject = 8e3;
-        var numberBall = 25;
-        var pulsion = 16;
-        var speedX = 5;
-        var speedY = 2;
-        var colorRect = "rgba(255,255,255,0.05)";
-        var opacityRect = .2;
-        var rint = 30;
-        var radius = 100;
-        $(document).ready(function() {
-BrowserDetection();
-        WIDTH = window.innerWidth;
-        HEIGHT = window.innerHeight;
-        canvas = document.getElementById("pixie");
-        $(canvas).attr("width", WIDTH).attr("height", HEIGHT);
-        con = canvas.getContext("2d");
-        for (var e = 0; e < numberBall; e++) {
-pxs[e] = new Circle;
-        pxs[e].reset()
-        }
-setInterval(draw, rint)
-})
+$(function() {
+    $("#login").submit(function() {
+        $(".panel-sign-in").addClass("hidden"), $(".panel-sign-in-loader").removeClass("hidden");
+        var t = $(this);
+        return $.ajax({
+            url: "DoAuth",
+            type: "POST",
+            data: t.serialize(),
+            success: function(t) {
+                console.log(t), 1 === t.responseCode && !0 === t.doauth ? ($.notify("Redirecting", "success"), window.location.href = contextPath + "/views/" + t.role + "/") : ($.notify("Invalid Credentials", "error"), $(".panel-sign-in").removeClass("hidden"), $(".panel-sign-in-loader").addClass("hidden"))
+            }
+        }), !1
+    })
+});
+var pxs = new Array,
+    numberObject = 8e3,
+    numberBall = 25,
+    pulsion = 16,
+    speedX = 5,
+    speedY = 2,
+    colorRect = "rgba(255,255,255,0.05)",
+    opacityRect = .2,
+    rint = 30,
+    radius = 100;
+$(document).ready(function() {
+    BrowserDetection(), WIDTH = window.innerWidth, HEIGHT = window.innerHeight, canvas = document.getElementById("pixie"), $(canvas).attr("width", WIDTH).attr("height", HEIGHT), con = canvas.getContext("2d");
+    for (var t = 0; t < numberBall; t++) pxs[t] = new Circle, pxs[t].reset();
+    setInterval(draw, rint)
+});
--- src/main/webapp/Assets/js/user-customerChat.js
+++ src/main/webapp/Assets/js/user-customerChat.js
@@ -1,65 +1,33 @@
-$(function () {
-
-
+$(function() {
     $.ajax({
         url: "../../FetchCustomerChats",
         method: "POST",
-        success: function (data, textStatus, jqXHR) {
-//            console.log(data);
-            if (data.responseCode == 1) {
-                for (var i in data.tcsEmailMappedCustomerData) {
-                    data.tcsEmailMappedCustomerData[i].tcsEmailContent = data.tcsEmailMappedCustomerData[i].tcsEmailContent.replace("ú", "<br>");
-                    $(".card-email-thread").append(templatetohtml("addProject", data.tcsEmailMappedCustomerData[i]));
-
-                }
-                for (var i in data.tcsEmailMasterEmailStatus) {
-                    $("#status").append(templatetohtml("addMasterStatus", data.tcsEmailMasterEmailStatus[i]));
-
-                }
-            } else {
-                errorElsePart();
-            }
-        }, error: function (jqXHR, textStatus, errorThrown) {
-            console.log(errorThrown);
-
+        success: function(a, t, e) {
+            if (1 === a.responseCode) {
+                for (var s in a.tcsEmailMappedCustomerData) a.tcsEmailMappedCustomerData[s].tcsEmailContent = a.tcsEmailMappedCustomerData[s].tcsEmailContent.replace("ú", "<br>"), $(".card-email-thread").append(templatetohtml("addProject", a.tcsEmailMappedCustomerData[s]));
+                for (var s in a.tcsEmailMasterEmailStatus) $("#status").append(templatetohtml("addMasterStatus", a.tcsEmailMasterEmailStatus[s]));
+            } else errorElsePart();
+        },
+        error: function(a, t, e) {
+           errorBlock();
         }
-    });
-
-    $("#saveMail").submit(function () {
-        if ($("#status").val() != null) {
+    }), $("#saveMail").submit(function() {
+        if (null !== $("#status").val()) {
             $(".panel-sign-in-loader").removeClass("hidden");
-            var $form = $(this);
+            var a = $(this);
             $.ajax({
                 url: "../../SaveMailActivity",
-                type: 'POST',
-                data: $form.serialize(),
-                success: function (data) {
-                    if (data.responseCode == 1) {
+                type: "POST",
+                data: a.serialize(),
+                success: function(a) {
+                    if (1 === a.responseCode) {
                         $(".card-email-thread").empty();
-                        for (var i in data.tcsEmailMappedCustomerData) {
-                            data.tcsEmailMappedCustomerData[i].tcsEmailContent = data.tcsEmailMappedCustomerData[i].tcsEmailContent.replace("ú", "<br>");
-                            $(".card-email-thread").append(templatetohtml("addProject", data.tcsEmailMappedCustomerData[i]));
-
-                        }
-                        $("#cc").val("");
-                        $("#subject").val("");
-                        $("#date").val("");
-                        $("#status").val(0);
-                        $("#content").val("");
-                        DataSaved();
-                    } else if (data.responseCode == 2) {
-                        nullValues();
-                    } else {
-                        errorBlock();
-                    }
+                        for (var t in a.tcsEmailMappedCustomerData) a.tcsEmailMappedCustomerData[t].tcsEmailContent = a.tcsEmailMappedCustomerData[t].tcsEmailContent.replace("ú", "<br>"), $(".card-email-thread").append(templatetohtml("addProject", a.tcsEmailMappedCustomerData[t]));
+                        $("#cc").val(""), $("#subject").val(""), $("#date").val(""), $("#status").val(0), $("#content").val(""), DataSaved()
+                    } else 2 === a.responseCode ? nullValues() : errorBlock();
                 }
             });
-        } else {
-
-
-
-            $.notify("Please select Status", "warn");
-        }
-        return false;
+        } else $.notify("Please select Status", "warn");
+        return !1;
     });
 });
